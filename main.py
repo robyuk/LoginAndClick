ac7901e430376528a44cb32b82282f0ed4578be4
robertay <94378311+robyuk@users.noreply.github.com>
2022-01-11 12:55:26 +0000

    reverted

--- main.py
+++ main.py
@@ -3,52 +3,56 @@ from selenium.webdriver.common.keys import Keys
 import time
 from datetime import datetime as dt
 
-rywebpage = 'https://titan22.com/account/login?return_url=%2Faccount'
-loginfield = 'CustomerEmail'
-pwfield = 'CustomerPassword'
-loginname = 'youngr2000-titan22@yahoo.com'
-password = 'PnQf8hVJanCBhxiLCCw5'
-clickme = '//*[@id="shopify-section-footer"]/section/div/div[1]/div[1]/div[1]/nav/ul/li[1]/a'
-
+rywebpage='http://automated.pythonanywhere.com/login/'
+loginfield='id_username'
+pwfield='id_password'
+loginname='automated'
+password='automatedautomated'
+clickme='/html/body/nav/div/a'
+id='displaytimer'
+filename='temp.txt'
 
 def chromedriver():
-    #Set options to make web browsing easier
-    options = webdriver.ChromeOptions()
-    options.add_argument('disable-infobars')
-    options.add_argument('start-maximized')
-    options.add_argument('disable-dev-shm-usage')
-    options.add_argument('no-sandbox')
-    options.add_argument('disable-blink-features=AutomationControlled')
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
-
-    driver = webdriver.Chrome(options=options)
-    return driver
-
+  #Set options to make web browsing easier
+  options=webdriver.ChromeOptions()
+  options.add_argument('disable-infobars')
+  options.add_argument('start-maximized')
+  options.add_argument('disable-dev-shm-usage')
+  options.add_argument('no-sandbox')
+  options.add_argument('disable-blink-features=AutomationControlled')
+  options.add_experimental_option('excludeSwitches', ['enable-automation'])
+  
+  driver=webdriver.Chrome(options=options)
+  return driver
 
 def gettemp(text):
-    """ Extract only the temperature from the text string """
-    temp = float(text.split(": ")[1])
-    return temp
+  """ Extract only the temperature from the text string """
+  temp=float(text.split(": ")[1])
+  return temp
 
+def pagelogin(rywebpage,loginfield,loginname,pwfield,password):
+  """ Logs in to a web page """
+  driver=chromedriver()
+  driver.get(rywebpage)
+  driver.find_element(by='id', value=loginfield).send_keys(loginname)
+  time.sleep(2)
+  driver.find_element(by='id', value=pwfield).send_keys(password+Keys.RETURN)
+  time.sleep(2)
+  return driver
 
-def pagelogin(rywebpage, loginfield, loginname, pwfield, password):
-    """ Logs in to a web page """
-    driver = chromedriver()
-    driver.get(rywebpage)
-    driver.find_element(by='id', value=loginfield).send_keys(loginname)
+def main(rywebpage,loginfield,loginname,pwfield,password,clickme,id):
+  """Log into a webpage, click a link, and scrape a value to a file"""
+  driver=pagelogin(rywebpage,loginfield,loginname,pwfield,password)
+  driver.find_element(by='xpath', value=clickme).click
+
+  while True:
+    with open(filename,'a') as file:
+      timestamp=str(dt.now())
+      element=driver.find_element(by='id', value=id)
+      file.write(f'{timestamp.split(".")[0]} : {gettemp(element.text)}\n')
     time.sleep(2)
-    driver.find_element(by='id',
-                        value=pwfield).send_keys(password + Keys.RETURN)
-    time.sleep(2)
-    return driver
+
+  return element
 
 
-def main(rywebpage, loginfield, loginname, pwfield, password, clickme):
-    """Log into a webpage, click a link, and scrape a value to a file"""
-    driver = pagelogin(rywebpage, loginfield, loginname, pwfield, password)
-    time.sleep(19)
-    driver.find_element(by='xpath', value=clickme).click
-    print(driver.current_url)
-
-    #
-print(main(rywebpage, loginfield, loginname, pwfield, password, clickme))
+print(gettemp(main(rywebpage,loginfield,loginname,pwfield,password,clickme,id).text))
